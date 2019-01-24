6127c2ee8d11eda75a9089d7e7ef311408935d71
Matthew Matl <matthewcmatl@gmail.com>
2019-01-24 15:40:02 -0800

    WIP

--- urdfpy/tmp.py
+++ urdfpy/tmp.py
@@ -16,56 +16,60 @@ def _parse(node, name, val_type, many=False, required=False):
         else:
             return val_type._from_xml(val[-1])
 
-class URDFType(object):
-
-    TAG = ''       # String tag of node
-    LINK = False
-    ATTRIBS = []    # Simple, list of str
-    ELEMENTS = {}   # Map from attrib name to tuple containing Type, Required, Many
-
-    def __init__(self):
-        pass
-
-    @classmethod
-    def _parse(cls, node):
-        kwargs = {}
-
-        # Get attributes
-        for a in cls.ATTRIBS:
-            kwargs[a] = node.attrib[a]
-
-        # Set elements
-        for e in cls.ELEMENTS:
-            vtype, required, many = cls.ELEMENTS[e]
-            values = node.findall(e)
-            if required and len(values) == 0:
-                pn = ''
-                if 'name' in node.attrib:
-                    pn = node.attrib['name']
-                raise ValueError('Missing required field {} in {} "{}"'.format(name, pn, node.tag))
-            if many or len(values) == 0:
-                kwargs[e] = [vtype._parse(v) for v in values]
-            else:
-                return vtype._parse(values[-1])
-
-        kwargs = cls._postparse(kwargs)
-
-        return cls(**kwargs)
-
-    @classmethod
-    def _postparse(cls, kwargs):
-        return kwargs
-
-    def _serialize(self, parent):
-        node = ET.Element(self.TAG)
-
-        # Set attributes
-        for a in self.ATTRIBS:
-            node.attrib[a] = getattr(self, a, Nonde)
-
-        # Get elements
-        for e in self.ELEMENTS:
-            pass
+#class URDFType(object):
+#
+#    TAG = ''        # String tag of node
+#    ATTRIBS = []    # Simple, list of str
+#    ELEMENTS = {}   # Map from attrib name to tuple containing Type, Required, Many
+#    SPECIALS = {}   # Map from special attribs to Function, Requred, ?
+#
+#    def __init__(self):
+#        pass
+#
+#    @classmethod
+#    def _parse(cls, node):
+#        kwargs = {}
+#
+#        # Get attributes
+#        for a in cls.ATTRIBS:
+#            kwargs[a] = node.attrib[a]
+#
+#        # Set elements
+#        for e in cls.ELEMENTS:
+#            vtype, required, many = cls.ELEMENTS[e]
+#            values = node.findall(e)
+#            if required and len(values) == 0:
+#                pn = ''
+#                if 'name' in node.attrib:
+#                    pn = node.attrib['name']
+#                raise ValueError('Missing required field {} in {} "{}"'.format(name, pn, node.tag))
+#            if many or len(values) == 0:
+#                kwargs[e] = [vtype._parse(v) for v in values]
+#            else:
+#                return vtype._parse(values[-1])
+#
+#        # Set specials
+#        for e in cls.SPECIALS:
+#            kwargs[e] = cls.SPECIALS[e](
+#
+#        kwargs = cls._postparse(kwargs)
+#
+#        return cls(**kwargs)
+#
+#    @classmethod
+#    def _postparse(cls, kwargs):
+#        return kwargs
+#
+#    def _serialize(self, parent):
+#        node = ET.Element(self.TAG)
+#
+#        # Set attributes
+#        for a in self.ATTRIBS:
+#            node.attrib[a] = getattr(self, a, Nonde)
+#
+#        # Get elements
+#        for e in self.ELEMENTS:
+#            pass
 
 
 
@@ -79,28 +83,150 @@ class JointDynamics(object):
         self.friction = friction
 
 class Box(object):
+
     def __init__(self, size=None):
         self.size = size
 
+    @staticmethod
+    def _from_xml(self, node, path):
+        size = np.fromstring(node.attrib['size'], sep=' ')
+        return Box(size=size)
+
+    def _to_xml(self, path):
+        node = ET.Element('box')
+        node.attrib['size'] = np.array2string(self.size)[1:-1]
+        return node
+
 class Cylinder(object):
-    def __init__(self, radius=0.0, length=0.0):
+
+    def __init__(self, radius, length):
         self.radius = radius
         self.length = length
 
+    @staticmethod
+    def _from_xml(self, node, path):
+        radius = float(node.attrib['radius'])
+        length = float(node.attrib['length'])
+        return Cylinder(radius, length)
+
+    def _to_xml(self, path):
+        node = ET.Element('cylinder')
+        node.attrib['radius'] = str(self.radius)
+        node.attrib['length'] = str(self.length)
+        return node
+
 class Sphere(object):
-    def __init__(self, radius=0.0):
+
+    def __init__(self, radius):
         self.radius = radius
 
+    @staticmethod
+    def _from_xml(self, node, path):
+        radius = float(node.attrib['radius'])
+        return Sphere(radius)
+
+    def _to_xml(self, path):
+        node = ET.Element('cylinder')
+        node.attrib['radius'] = str(self.radius)
+        return node
+
 class Mesh(object):
-    def __init__(self, filename=None, scale=None):
+
+    def __init__(self, filename, scale, mesh):
         self.filename = filename
         self.scale = scale
+        self.mesh = mesh
+
+    @staticmethod
+    def _from_xml(self, node, path):
+        filename = node.attrib['filename']
+        scale = 1.0
+        if 'scale' in node.attrib:
+            scale = float(node.attrib['scale'])
+        mesh = trimesh.load(os.path.join(path, filename))
+        return Mesh(filename=filename, scale=scale, mesh=mesh)
+
+    def _to_xml(self, path):
+        node = ET.Element('mesh')
+        node.attrib['filename'] = str(self.filename)
+        node.attrib['scale'] = str(self.scale)
+        self.mesh.export(os.path.join(path, self.filename))
+        return node
+
+class Geometry(object):
+
+    def __init__(self, box=None, cylinder=None, sphere=None, mesh=None):
+        self.box = box
+        self.cylinder = cylinder
+        self.sphere = sphere
+        self.mesh = mesh
+
+    @staticmethod
+    def _from_xml(self, node, path):
+        box, cylinder, sphere, mesh = None, None, None, None
+        children = []
+        for child in node:
+            children.append(child)
+        child = children[-1]
+        if child.tag == 'box':
+            box = Box._from_xml(child, path)
+        elif child.tag == 'cylinder':
+            cylinder = Cylinder._from_xml(child, path)
+        elif child.tag == 'sphere':
+            sphere = Sphere._from_xml(child, path)
+        elif child.tag == 'mesh':
+            mesh = Mesh._from_xml(child, path)
+        return Geometry(box, cylinder, sphere, mesh)
+
+    @staticmethod
+    def _to_xml(self, path):
+        node = ET.Element('geometry')
+        if self.box is not None:
+            node.append(self.box._to_xml(path))
+        elif self.cylinder is not None:
+            node.append(self.cylinder._to_xml(path))
+        elif self.sphere is not None:
+            node.append(self.sphere._to_xml(path))
+        elif self.mesh is not None:
+            node.append(self.mesh._to_xml(path))
+        return node
 
 class Collision(object):
 
-    def __init__(self, geometry=None, origin=None):
+    def __init__(self, name=None, origin=None, geometry=None):
+        self.name = name
         self.geometry = geometry
         self.origin = origin
+
+    @staticmethod
+    def _from_xml(self, node, path):
+        name = None
+        if 'name' in node.attrib:
+            name = node.attrib['name']
+
+        origin = np.eye(4)
+        origin_node = node.find('origin')
+        if origin_node is not None:
+            if 'xyz' in origin_node.attrib:
+                origin[:3,3] = np.fromstring(origin_node.attrib['xyz'], sep=' ')
+            if 'rpy' in origin_node.attrib:
+                rpy = np.fromstring(origin_node.attrib['rpy'], sep=' ')
+        size = np.fromstring(node.attrib['size'], sep=' ')
+
+        filename = node.attrib['filename']
+        scale = 1.0
+        if 'scale' in node.attrib:
+            scale = float(node.attrib['scale'])
+        mesh = trimesh.load(os.path.join(path, filename))
+        return Mesh(filename=filename, scale=scale, mesh=mesh)
+
+    def _to_xml(self, path):
+        node = ET.Element('mesh')
+        node.attrib['filename'] = str(self.filename)
+        node.attrib['scale'] = str(self.scale)
+        self.mesh.export(os.path.join(path, self.filename))
+        return node
+
 
 class Texture(object):
     def __init__(self, filename=None):
